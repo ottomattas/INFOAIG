56613bbe9823f3c8bfff253d786d568dfa009fc8
ernogroeneweg@gmail.com <ernogroeneweg@gmail.com>
2019-10-25 15:16:53 +0200

    engine and SFP are now global within Agent class

--- Project/dl_queries/src/main/Agent.java
+++ Project/dl_queries/src/main/Agent.java
@@ -19,6 +19,9 @@ import org.semanticweb.owlapi.util.SimpleShortFormProvider;
 
 public class Agent {
 
+	static DLQueryEngine engine;
+	static ShortFormProvider shortFormProvider;
+	
 	public static void main(String[] args) throws Exception{
         String onto = new String(Files.readAllBytes(Paths.get("../courseOntology.owl")), "UTF-8");
         // Load an example ontology.
@@ -30,20 +33,25 @@ public class Agent {
         // This example uses HermiT: http://hermit-reasoner.com/
         OWLReasoner reasoner = new Reasoner.ReasonerFactory().createReasoner(ontology);
         // We also need a short form provider, to get the names of objects in the ontology from their IRIs
-        ShortFormProvider shortFormProvider = new SimpleShortFormProvider();
+        shortFormProvider = new SimpleShortFormProvider();
         
         // initialise a DL Query Engine, to query the ontology directly from here.
-        DLQueryEngine engine = new DLQueryEngine(reasoner, shortFormProvider);
+        engine = new DLQueryEngine(reasoner, shortFormProvider);
 
+
+        // System.out.println(dlQueryPrinter.getInstances("course and isTaughtBy value LECTURER_B"));
+        
+    	String query = "course and isTaughtBy value LECTURER_B";
+        testPrint(query);
+            
+	}
+	private static void testPrint(String query) {
         // Test print
         // This also shows how querying is done; we need the engine and the shortformprovider for it
-    	String query = "course and isTaughtBy value LECTURER_B";
         for (OWLEntity entity : engine.getInstances(query, false)) {
         	System.out.println(shortFormProvider.getShortForm(entity));
         }
         
-        // System.out.println(dlQueryPrinter.getInstances("course and isTaughtBy value LECTURER_B"));
-            
 	}
 }
 
